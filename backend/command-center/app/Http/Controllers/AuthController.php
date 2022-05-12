e9dbd35edf7e7cc75545adf06c03a3f787ad49e7
Sudipta Jana <sjgalaxy98@gmail.com>
2022-05-13 00:12:20 +0530

    forget password backend logic

--- backend/command-center/app/Http/Controllers/AuthController.php
+++ backend/command-center/app/Http/Controllers/AuthController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Exception;
+use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+
 
 class AuthController extends Controller
 {
@@ -41,5 +47,52 @@ class AuthController extends Controller
             'status' => 'success',
             'message' => 'Logout successfull',
         ]);
+    }
+
+    public function forgetPassword(Request $request)
+    {
+        try {
+            $request->validate(['email' => 'required|email']);
+
+            $status = Password::sendResetLink(
+                $request->only('email')
+            );
+
+            return $status === Password::RESET_LINK_SENT
+                ? response()->json(['status' => 'success', 'message' => __($status)])
+                : response()->json(['status' => 'error', 'message' => __($status)], 422);
+        } catch (Exception $e) {
+            return response()->json(['status' => 'error', 'message' => $e->getMessage()], 500);
+        }
+    }
+
+    public function ResetPassword(Request $request)
+    {
+        try {
+            $request->validate([
+                'token' => 'required',
+                'email' => 'required|email',
+                'password' => 'required|min:6|confirmed',
+            ]);
+
+            $status = Password::reset(
+                $request->only('email', 'password', 'password_confirmation', 'token'),
+                function ($user, $password) {
+                    $user->forceFill([
+                        'password' => Hash::make($password)
+                    ])->setRememberToken(Str::random(60));
+
+                    $user->save();
+
+                    event(new PasswordReset($user));
+                }
+            );
+
+            return $status === Password::PASSWORD_RESET
+                ? response()->json(['status' => 'success', 'message' => __($status)])
+                : response()->json(['status' => 'error', 'message' => __($status)]);
+        } catch (Exception $e) {
+            return response()->json(['status' => 'error', 'message' => $e->getMessage()], 500);
+        }
     }
 }
